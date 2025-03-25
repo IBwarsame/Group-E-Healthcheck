07bf4f4dfe9043a94306ae7e74fff7ced7f807f5
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-03-25 15:42:50 +0000

    temporarily disabled password validation

--- SEVENTYSEVENMARKS/settings.py
+++ SEVENTYSEVENMARKS/settings.py
@@ -85,18 +85,16 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    # ! PASSWORD VALIDATION TEMPORARILY DISABLED FOR TESTING PURPOSES
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
