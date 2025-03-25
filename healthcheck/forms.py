6c009e6613727d83c92b1cf7dbcaddf7c626f541
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-03-25 16:26:10 +0000

    add userprofile custom model with role

--- healthcheck/forms.py
+++ healthcheck/forms.py
@@ -1,10 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import UserProfile
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    role = forms.ChoiceField(
+        choices=UserProfile.ROLES, 
+        required=True,
+        initial='engineer'
+    )
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2", "role")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['role'].widget.attrs.update({
+            'class': 'input-field',
+        })
+
+    def save(self, commit=True):
+        user = super().save(commit=True)
+        user_profile = UserProfile.objects.create(
+            user=user,
+            role=self.cleaned_data['role']
+        )
+        return user
--- healthcheck/models.py
+++ healthcheck/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    ROLES = (
+        ('engineer', 'Engineer'),
+        ('teamLeader', 'Team Leader'),
+        ('departmentLeader', 'Department Leader'),
+        ('seniorManager', 'Senior Manager'),
+    )
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLES, default='engineer')
