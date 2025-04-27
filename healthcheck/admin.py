4a617503c72a58367b43f2e1fb6976405facaa51
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-04-27 21:04:39 +0100

    allow teams and sessions to be created by django admin

--- healthcheck/admin.py
+++ healthcheck/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Team, UserProfile, HealthCheckSession, Vote
 
 # Register your models here.
+
+admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    readonly_fields = ('created_at',)
+    
+
+@admin.register(HealthCheckSession)
+class HealthCheckSessionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('start_date', 'end_date')
+    readonly_fields = ('created_at',)
+    ordering = ('-start_date',)
